5bda925a308968902753fd9f0ee2b1021ca3bb10
IlyaAlpatov <alpatov30@gmail.com>
2023-03-16 22:14:29 +0300

    добавлен входной параметр в компонент

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import ShopItemFunc from "./components/ShopItemFunc";
 import "./App.css";
 
+const item = {
+  brand: "Tiger of Sweden",
+  title: "Leonard coat",
+  description: "Minimalistic coat in cotton-blend",
+  descriptionFull:
+    "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
+  price: 399,
+  currency: "£",
+};
+
 function App() {
   return (
     <div className="container">
@@ -9,7 +19,7 @@ function App() {
         <div className="highlight-overlay"></div>
       </div>
       <div className="window">
-        <ShopItemFunc />
+        <ShopItemFunc item={item} />
       </div>
     </div>
   );
--- src/components/ShopItemFunc.jsx
+++ src/components/ShopItemFunc.jsx
@@ -1,20 +1,21 @@
-const ShopItemFunc = () => {
+const ShopItemFunc = ({ item }) => {
+  const { brand, title, description, descriptionFull, price, currency } = item;
+
   return (
-    <div class="main-content">
-      <h2>Tiger of Sweden</h2>
-      <h1>Leonard coat</h1>
-      <h3>Minimalistic coat in cotton-blend</h3>
-      <div class="description">
-        Men's minimalistic overcoat in cotton-blend. Features a stand-up collar,
-        concealed front closure and single back vent. Slim fit with clean,
-        straight shape. Above-knee length.
+    <div className="main-content">
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
       </div>
-      <div class="highlight-window mobile">
-        <div class="highlight-overlay"></div>
-      </div>
-      <div class="divider"></div>
-      <div class="purchase-info">
-        <div class="price">£399.00</div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">
+          {currency}
+          {price}
+        </div>
         <button>Добавить в корзину</button>
       </div>
     </div>
